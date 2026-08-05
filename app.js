d27a26208929ebd65b7030b8f658debe89d05926
Shreya Chowdhury <125342052+Shreya831@users.noreply.github.com>
2026-08-05 13:32:27 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://giokargxqyjraxitbehi.supabase.co";
+const supabaseKey = "sb_publishable_LHNsm-LxWaN39CJa3_hcAA_jBdEgGrv";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
